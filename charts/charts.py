021365f99ab1f99b78376f92572d413a426304e4
Daniel <danielmontrest@gmail.com>
2022-10-30 12:00:29 -0500

    Mensaje

--- charts/charts.py
+++ charts/charts.py
@@ -4,7 +4,8 @@ def generate_pie_chart():
     labels = ["A", "B", "C"]
     values = [200, 40, 70]
 
-    fig, ax = plt.subplotes()
+    fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
     plt.savefig("pie.png")
+    plt.show()
     plt.close()
